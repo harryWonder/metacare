8d27e80fc5f253a997ee9ed1fb84ba8d677d620a
Ilori Stephen <stephenilori458@gmail.com>
2022-01-20 00:25:40 +0100

    Added Extra ENV KEY/VALUE Pair

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-console.log(process.env.DB_HOST);
-
 module.exports = {
   development: {
     "username": process.env.DB_USERNAME,
@@ -21,7 +19,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    host: process.env.DATABASE_HOST,
     dialect: 'mysql',
   },
 };
